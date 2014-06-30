e34b404fc7b4ee7dbc8b4aa2ae942f3bca1d971c
katherineabuhadal <katherine8@verizon.net>
2014-06-30 09:56:31 -0400

    add sign-in/up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
+
 Rails.application.routes.draw do
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
+
+  root to: "sessions#new"
+   get "/sign-up" to: "users#new"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
